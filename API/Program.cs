6f1ced561b8bebb00b5ca59e37fa18b545615bb1
Apozzz <bosas.arnas2000@gmail.com>
2023-10-25 23:05:16 +0300

    Created Simple CRUD for Activities

--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,10 @@
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -20,7 +14,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 
 var summaries = new[]
@@ -57,6 +50,9 @@ catch (Exception ex)
     logger.LogError(ex, "An error occured during migration");
 }
 
+app.UseCors("CorsPolicy");
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 app.Run();
 
